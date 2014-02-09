cf97968e269d9c937b35fbd0e6d7ea99ce4106b1
EC2 Default User <ec2-user@ip-10-17-48-112.ec2.internal>
2014-02-09 01:28:54 +0000

    update

--- mpi/mpi_io.c
+++ mpi/mpi_io.c
@@ -179,125 +179,125 @@ int mpi_write(int        isOutFileBinary, /* flag: 0 or 1 */
               MPI_Comm   comm,
               int        verbose)
 {
-    int        divd, rem, len, err;
-    int        i, j, k, rank, nproc;
-    char       outFileName[1024], fs_type[32], str[32], *delim;
-    MPI_File   fh;
-    MPI_Status status;
+    //int        divd, rem, len, err;
+    //int        i, j, k, rank, nproc;
+    //char       outFileName[1024], fs_type[32], str[32], *delim;
+    //MPI_File   fh;
+    //MPI_Status status;
 
-    MPI_Comm_rank(comm, &rank);
-    MPI_Comm_size(comm, &nproc);
+    //MPI_Comm_rank(comm, &rank);
+    //MPI_Comm_size(comm, &nproc);
 
-    delim = strchr(filename, ':');
-    if (delim != NULL) {
-        strncpy(fs_type, filename, delim-filename);
-        fs_type[delim-filename] = '\0';
-        /* real file name starts from delim+1 */
-        delim++;
-    }
-    else
-        delim = filename;
+    //delim = strchr(filename, ':');
+    //if (delim != NULL) {
+    //    strncpy(fs_type, filename, delim-filename);
+    //    fs_type[delim-filename] = '\0';
+    //    /* real file name starts from delim+1 */
+    //    delim++;
+    //}
+    //else
+    //    delim = filename;
 
-    /* output: the coordinates of the cluster centres ----------------------*/
-    /* only proc 0 do this, because clusters[] are the same across all proc */
-    if (rank == 0) {
-        if (verbose) printf("Writing coordinates of K=%d cluster centers to file \"%s.cluster_centres\"\n",
-                            numClusters, delim);
-        sprintf(outFileName, "%s.cluster_centres", filename);
-        err = MPI_File_open(MPI_COMM_SELF, outFileName, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
-        if (err != MPI_SUCCESS) {
-            char errstr[MPI_MAX_ERROR_STRING];
-            int  errlen;
-            MPI_Error_string(err, errstr, &errlen);
-            printf("Error at opening file %s (%s)\n", outFileName,errstr);
-            MPI_Finalize();
-            exit(1);
-        }
-        if (isOutFileBinary) {
-            MPI_File_write(fh, &numClusters, 1, MPI_INT, &status);
-            MPI_File_write(fh, &numCoords,   1, MPI_INT, &status);
-            MPI_File_write(fh, clusters[0], numClusters*numCoords, MPI_FLOAT, &status);
-        }
-        else {
-            for (i=0; i<numClusters; i++) {
-                sprintf(str, "%d ", i);
-                MPI_File_write(fh, str, strlen(str), MPI_CHAR, &status);
-                for (j=0; j<numCoords; j++) {
-                    sprintf(str, "%f ", clusters[i][j]);
-                    MPI_File_write(fh, str, strlen(str), MPI_CHAR, &status);
-                }
-                MPI_File_write(fh, "\n", 1, MPI_CHAR, &status);
-            }
-        }
-        MPI_File_close(&fh);
-    }
+    ///* output: the coordinates of the cluster centres ----------------------*/
+    ///* only proc 0 do this, because clusters[] are the same across all proc */
+    //if (rank == 0) {
+    //    if (verbose) printf("Writing coordinates of K=%d cluster centers to file \"%s.cluster_centres\"\n",
+    //                        numClusters, delim);
+    //    sprintf(outFileName, "%s.cluster_centres", filename);
+    //    err = MPI_File_open(MPI_COMM_SELF, outFileName, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
+    //    if (err != MPI_SUCCESS) {
+    //        char errstr[MPI_MAX_ERROR_STRING];
+    //        int  errlen;
+    //        MPI_Error_string(err, errstr, &errlen);
+    //        printf("Error at opening file %s (%s)\n", outFileName,errstr);
+    //        MPI_Finalize();
+    //        exit(1);
+    //    }
+    //    if (isOutFileBinary) {
+    //        MPI_File_write(fh, &numClusters, 1, MPI_INT, &status);
+    //        MPI_File_write(fh, &numCoords,   1, MPI_INT, &status);
+    //        MPI_File_write(fh, clusters[0], numClusters*numCoords, MPI_FLOAT, &status);
+    //    }
+    //    else {
+    //        for (i=0; i<numClusters; i++) {
+    //            sprintf(str, "%d ", i);
+    //            MPI_File_write(fh, str, strlen(str), MPI_CHAR, &status);
+    //            for (j=0; j<numCoords; j++) {
+    //                sprintf(str, "%f ", clusters[i][j]);
+    //                MPI_File_write(fh, str, strlen(str), MPI_CHAR, &status);
+    //            }
+    //            MPI_File_write(fh, "\n", 1, MPI_CHAR, &status);
+    //        }
+    //    }
+    //    MPI_File_close(&fh);
+    //}
 
-    /* output: the closest cluster centre to each of the data points --------*/
-    if (rank == 0 && verbose)
-        printf("Writing membership of N=%d data objects to file \"%s.membership\"\n",
-               totalNumObjs, delim);
+    ///* output: the closest cluster centre to each of the data points --------*/
+    //if (rank == 0 && verbose)
+    //    printf("Writing membership of N=%d data objects to file \"%s.membership\"\n",
+    //           totalNumObjs, delim);
 
-    if (isOutFileBinary) {
-        sprintf(outFileName, "%s.membership", filename);
-        err = MPI_File_open(comm, outFileName, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
-        if (err != MPI_SUCCESS) {
-            char errstr[MPI_MAX_ERROR_STRING];
-            int  errlen;
-            MPI_Error_string(err, errstr, &errlen);
-            printf("Error at opening file %s (%s)\n", outFileName,errstr);
-            MPI_Finalize();
-            exit(1);
-        }
-        /* write numObjs from the 1st integer */
-        if (rank == 0)
-            MPI_File_write(fh, &totalNumObjs, 1, MPI_INT, &status);
+    //if (isOutFileBinary) {
+    //    sprintf(outFileName, "%s.membership", filename);
+    //    err = MPI_File_open(comm, outFileName, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
+    //    if (err != MPI_SUCCESS) {
+    //        char errstr[MPI_MAX_ERROR_STRING];
+    //        int  errlen;
+    //        MPI_Error_string(err, errstr, &errlen);
+    //        printf("Error at opening file %s (%s)\n", outFileName,errstr);
+    //        MPI_Finalize();
+    //        exit(1);
+    //    }
+    //    /* write numObjs from the 1st integer */
+    //    if (rank == 0)
+    //        MPI_File_write(fh, &totalNumObjs, 1, MPI_INT, &status);
 
-        divd = totalNumObjs / nproc;
-        rem  = totalNumObjs % nproc;
-        len  = (rank < rem) ? rank*(divd+1) : rank*divd + rem;
+    //    divd = totalNumObjs / nproc;
+    //    rem  = totalNumObjs % nproc;
+    //    len  = (rank < rem) ? rank*(divd+1) : rank*divd + rem;
 
-        MPI_Offset disp = (len + 1) * sizeof(int);
-        MPI_File_seek(fh, disp, MPI_SEEK_SET);
-        MPI_File_write(fh, membership, numObjs, MPI_INT, &status);
-        MPI_File_close(&fh);
-    }
-    else {
-        if (rank == 0) { /* gather membership[] from all processes ----------*/
-            int divd = totalNumObjs / nproc;
-            int rem  = totalNumObjs % nproc;
+    //    MPI_Offset disp = (len + 1) * sizeof(int);
+    //    MPI_File_seek(fh, disp, MPI_SEEK_SET);
+    //    MPI_File_write(fh, membership, numObjs, MPI_INT, &status);
+    //    MPI_File_close(&fh);
+    //}
+    //else {
+    //    if (rank == 0) { /* gather membership[] from all processes ----------*/
+    //        int divd = totalNumObjs / nproc;
+    //        int rem  = totalNumObjs % nproc;
 
-            sprintf(outFileName, "%s.membership", filename);
-            err = MPI_File_open(MPI_COMM_SELF, outFileName, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
-            if (err != MPI_SUCCESS) {
-                char errstr[MPI_MAX_ERROR_STRING];
-                int  errlen;
-                MPI_Error_string(err, errstr, &errlen);
-                printf("Error at opening file %s (%s)\n", outFileName,errstr);
-                MPI_Finalize();
-                exit(1);
-            }
+    //        //sprintf(outFileName, "%s.membership", filename);
+    //        //err = MPI_File_open(MPI_COMM_SELF, outFileName, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
+    //        //if (err != MPI_SUCCESS) {
+    //        //    char errstr[MPI_MAX_ERROR_STRING];
+    //        //    int  errlen;
+    //        //    MPI_Error_string(err, errstr, &errlen);
+    //        //    printf("Error at opening file %s (%s)\n", outFileName,errstr);
+    //        //    MPI_Finalize();
+    //        //    exit(1);
+    //        //}
 
-            /* first, print out local membership[] */
-            for (j=0; j<numObjs; j++) {
-                sprintf(str, "%d %d\n", j, membership[j]);
-                MPI_File_write(fh, str, strlen(str), MPI_CHAR, &status);
-            }
+    //        ///* first, print out local membership[] */
+    //        //for (j=0; j<numObjs; j++) {
+    //        //    sprintf(str, "%d %d\n", j, membership[j]);
+    //        //    MPI_File_write(fh, str, strlen(str), MPI_CHAR, &status);
+    //        //}
 
-            k = numObjs;
-            for (i=1; i<nproc; i++) {
-                numObjs = (i < rem) ? divd+1 : divd;
-                MPI_Recv(membership, numObjs, MPI_INT, i, i, comm, &status);
+    //        //k = numObjs;
+    //        //for (i=1; i<nproc; i++) {
+    //        //    numObjs = (i < rem) ? divd+1 : divd;
+    //        //    MPI_Recv(membership, numObjs, MPI_INT, i, i, comm, &status);
 
-                for (j=0; j<numObjs; j++) {
-                    sprintf(str, "%d %d\n", k++, membership[j]);
-                    MPI_File_write(fh, str, strlen(str), MPI_CHAR, &status);
-                }
-            }
-            MPI_File_close(&fh);
-        }
-        else {
-            MPI_Send(membership, numObjs, MPI_INT, 0, rank, comm);
-        }
-    }
-    return 1;
+    //        //    for (j=0; j<numObjs; j++) {
+    //        //        sprintf(str, "%d %d\n", k++, membership[j]);
+    //        //        MPI_File_write(fh, str, strlen(str), MPI_CHAR, &status);
+    //        //    }
+    //        //}
+    //        //MPI_File_close(&fh);
+    //    }
+    //    else {
+    //        MPI_Send(membership, numObjs, MPI_INT, 0, rank, comm);
+    //    }
+    //}
+    //return 1;
 }
