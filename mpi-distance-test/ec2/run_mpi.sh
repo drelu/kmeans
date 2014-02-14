9d6d705307e2f68f170217ef4fa23dc47c9fbb2b
root <root@ip-10-67-134-243.ec2.internal>
2014-02-14 02:07:04 +0000

    some test data

---
+++ mpi-distance-test/ec2/run_mpi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ 
+time mpirun -np 24 -machinefile  ~/hostfile /home/ec2-user/kmeans/mpi/mpi_main -i ~/input-mpi/random_1000000points.csv -o -n 50000 -d #2>&1 | tee -a run.log
+time mpirun -np 24 -machinefile  ~/hostfile /home/ec2-user/kmeans/mpi/mpi_main -i ~/input-mpi/random_10000000points.csv -o -n 5000 -d #2>&1 | tee -a run.log
+time mpirun -np 24 -machinefile  ~/hostfile /home/ec2-user/kmeans/mpi/mpi_main -i ~/input-mpi/random_100000000points.csv -o -n 500 -d #2>&1 | tee -a run.log
---
+++ mpi-distance-test/seq_kmeans.c
@@ -0,0 +1,134 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*   File:         seq_kmeans.c  (sequential version)                        */
+/*   Description:  Implementation of simple k-means clustering algorithm     */
+/*                 This program takes an array of N data objects, each with  */
+/*                 M coordinates and performs a k-means clustering given a   */
+/*                 user-provided value of the number of clusters (K). The    */
+/*                 clustering results are saved in 2 arrays:                 */
+/*                 1. a returned array of size [K][N] indicating the center  */
+/*                    coordinates of K clusters                              */
+/*                 2. membership[N] stores the cluster center ids, each      */
+/*                    corresponding to the cluster a data object is assigned */
+/*                                                                           */
+/*   Author:  Wei-keng Liao                                                  */
+/*            ECE Department, Northwestern University                        */
+/*            email: wkliao@ece.northwestern.edu                             */
+/*                                                                           */
+/*   Copyright (C) 2005, Northwestern University                             */
+/*   See COPYRIGHT notice in top-level directory.                            */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "kmeans.h"
+#include "time.h"
+
+
+/*----< euclid_dist_2() >----------------------------------------------------*/
+/* square of Euclid distance between two multi-dimensional points            */
+__inline static
+float euclid_dist_2(int    numdims,  /* no. dimensions */
+                    float *coord1,   /* [numdims] */
+                    float *coord2)   /* [numdims] */
+{
+    int i;
+    float ans=0.0;
+
+    for (i=0; i<numdims; i++)
+        ans += (coord1[i]-coord2[i]) * (coord1[i]-coord2[i]);
+
+    return(ans);
+}
+
+/*----< find_nearest_cluster() >---------------------------------------------*/
+__inline static
+int find_nearest_cluster(int     numClusters, /* no. clusters */
+                         int     numCoords,   /* no. coordinates */
+                         float  *object,      /* [numCoords] */
+                         float **clusters)    /* [numClusters][numCoords] */
+{
+    int   index, i;
+    float dist, min_dist;
+
+    /* find the cluster id that has min distance to object */
+    index    = 0;
+    min_dist = euclid_dist_2(numCoords, object, clusters[0]);
+
+    for (i=1; i<numClusters; i++) {
+        dist = euclid_dist_2(numCoords, object, clusters[i]);
+        /* no need square root */
+        if (dist < min_dist) { /* find the min and its array index */
+            min_dist = dist;
+            index    = i;
+        }
+    }
+    return(index);
+}
+
+
+/*----< seq_kmeans() >-------------------------------------------------------*/
+/* return an array of cluster centers of size [numClusters][numCoords]       */
+float** seq_kmeans(float **objects,      /* in: [numObjs][numCoords] */
+                   int     numCoords,    /* no. features */
+                   int     numObjs,      /* no. objects */
+                   int     numClusters,  /* no. clusters */
+                   float   threshold,    /* % objects change membership */
+                   int    *membership)   /* out: [numObjs] */
+{
+    int      i, j, index, loop=0;
+    int     *newClusterSize; /* [numClusters]: no. objects assigned in each
+                                new cluster */
+    float    delta;          /* % of objects change their clusters */
+    float  **clusters;       /* out: [numClusters][numCoords] */
+    float  **newClusters;    /* [numClusters][numCoords] */
+
+    /* allocate a 2D space for returning variable clusters[] (coordinates
+       of cluster centers) */
+    clusters    = (float**) malloc(numClusters *             sizeof(float*));
+    assert(clusters != NULL);
+    clusters[0] = (float*)  malloc(numClusters * numCoords * sizeof(float));
+    assert(clusters[0] != NULL);
+    for (i=1; i<numClusters; i++)
+        clusters[i] = clusters[i-1] + numCoords;
+
+    /* pick first numClusters elements of objects[] as initial cluster centers*/
+    for (i=0; i<numClusters; i++)
+        for (j=0; j<numCoords; j++)
+            clusters[i][j] = objects[i][j];
+
+    /* initialize membership[] */
+    for (i=0; i<numObjs; i++) membership[i] = -1;
+
+    /* need to initialize newClusterSize and newClusters[0] to all 0 */
+    newClusterSize = (int*) calloc(numClusters, sizeof(int));
+    assert(newClusterSize != NULL);
+
+    newClusters    = (float**) malloc(numClusters *            sizeof(float*));
+    assert(newClusters != NULL);
+    newClusters[0] = (float*)  calloc(numClusters * numCoords, sizeof(float));
+    assert(newClusters[0] != NULL);
+    for (i=1; i<numClusters; i++)
+        newClusters[i] = newClusters[i-1] + numCoords;
+
+    delta = 0.0;
+    clock_t start = clock();
+    for (i=0; i<numObjs; i++) {
+        /* find the array index of nestest cluster center */
+        index = find_nearest_cluster(numClusters, numCoords, objects[i],
+                                     clusters);
+
+    }
+    clock_t end = clock();
+    double time_elapsed_in_seconds = (end - start)/(double)CLOCKS_PER_SEC;
+    printf("Time to iterate through %d points: %.3f", numObjs, time_elapsed_in_seconds);
+
+
+    free(newClusters[0]);
+    free(newClusters);
+    free(newClusterSize);
+
+    return clusters;
+}
+
